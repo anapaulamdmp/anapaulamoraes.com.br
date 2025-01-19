0097e1b0946d825b185a81706cfa7fa50c6e7132
Ana Paula Moraes <anapaula.mdmp@gmail.com>
2025-01-19 14:14:51 -0300

    Changed Project Page

--- src/app/[project]/page.tsx
+++ src/app/[project]/page.tsx
@@ -2,17 +2,11 @@ import Image from 'next/image'
 
 export async function generateStaticParams() {
   return [
-    { rbd21: '1' }, // Matches /project/1
-    { rbd21: '2' }, // Matches /project/2
-    { rbd21: '3' }, // Matches /project/3
+    { project: 'rbd-21' }, // Matches /project/rbd-21
   ];
 }
 
-export default function ProjectList() {
-  return (
-      <div className='h-screen flex flex-col w-screen'>
-        <span>Página de projetos</span>
-        
-      </div>
-  )
+export default function ProjectList({ params }: { params: { project: string } }) {
+  return <div>Project ID: {params.project}</div>;
 }
+
