98aa24400b4ed8799eb1ecedd080cd075faf6803
Ana Paula Moraes <anapaula.mdmp@gmail.com>
2025-01-22 15:19:12 -0300

    Project page

---
+++ src/app/[project]/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import Image from 'next/image'
+import { useState } from 'react'
+
+export default function ProjectPageList() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+      <div className='h-screen flex flex-col w-screen'>página de projetos</div>
+  )
+}
