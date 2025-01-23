95a8e7f5a0e9c538ec67b29ddf8b190977014e12
Ana Paula Moraes <anapaula.mdmp@gmail.com>
2025-01-23 09:44:55 -0300

    Mais um update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
  */
 const nextConfig = {
     output: 'export',
-   
+    images: { unoptimized: true }
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
    
@@ -12,6 +12,7 @@ const nextConfig = {
    
     // Optional: Change the output directory `out` -> `dist`
     // distDir: 'dist',
+    
   }
    
   module.exports = nextConfig
