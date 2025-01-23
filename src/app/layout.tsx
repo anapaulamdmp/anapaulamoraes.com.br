71c4f7c59bea407ed1c0fd3c09b923a7951feb23
Ana Paula Moraes <anapaula.mdmp@gmail.com>
2025-01-23 09:42:48 -0300

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,22 +15,27 @@ const manrope = Manrope({subsets: ['latin']});
 const libreBaskerville = Libre_Baskerville({
   subsets: ['latin'], // Choose appropriate subsets for your project
   weight: ['400', '700'], // Optional: Specify font weights (400 for normal, 700 for bold)
+  display: "swap"
 });
 const dmSerifText = DM_Serif_Text({
   subsets: ['latin'], // Add the appropriate subsets for your language
   weight: '400',      // DM Serif Text typically only has regular (400) weight
+  display: "swap"
 });
 const frankRuhlLibre = Frank_Ruhl_Libre({
   subsets: ['latin'], // Choose subsets based on your language
   weight: ['400', '500', '600', '700', '800'], // Optional: Specify weights (e.g., 400 for normal, 700 for bold)
+  display: "swap"
 });
 const caladea = Caladea({
   subsets: ['latin'],
   weight: ['400', '700'], // You can specify the weights you need
+  display: "swap"
 });
 const shrikhand = Shrikhand({
   subsets: ['latin'],
   weight: ['400'], // You can specify weights if needed, otherwise it defaults to 400
+  display: "swap"
 });
 
 
